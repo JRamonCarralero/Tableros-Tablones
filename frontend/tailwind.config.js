743c705138a53eebd825fec1eaec8241c4b371ce
JRamonCarralero <grukduz@gmail.com>
2025-07-17 13:15:51 +0200

    instalaciones de dependencias para el front

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+
+import PrimeUI from 'tailwindcss-primeui';
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+
+    },
+  },
+  plugins: [
+    PrimeUI
+  ],
+};
