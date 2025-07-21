175ba9701350ac3ed3e106280275eeda450a5333
JRamonCarralero <grukduz@gmail.com>
2025-07-22 00:29:24 +0200

    contacto en home

--- frontend/src/app/home/components/home/home.ts
+++ frontend/src/app/home/components/home/home.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Button } from "primeng/button";
 import { Footer } from "../../../footer/components/footer/footer";
+import { Image } from "primeng/image";
 
 @Component({
   selector: 'app-home',
-  imports: [Button, Footer],
+  imports: [Button, Footer, Image],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
+  emailAddress: string = 'informacion@tyt.com';
 }
