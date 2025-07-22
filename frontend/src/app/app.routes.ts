13361c9ede4b44279c42f5a66f8ccfd3d7df7e51
JRamonCarralero <grukduz@gmail.com>
2025-07-23 00:10:26 +0200

    Product List

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/components/home/home';
+import { ProductList } from './products/components/product-list/product-list';
 
 export const routes: Routes = [
-  { path: '', component: Home }
+  { path: '', component: Home },
+  { path: 'products', component: ProductList },
 ];
--- frontend/src/app/products/components/product-list/product-list.ts
+++ frontend/src/app/products/components/product-list/product-list.ts
@@ -1,11 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { ProductForm } from "../product-form/product-form";
+import { CommonModule } from '@angular/common';
+import { ProductModel } from '../../models/product-model';
+import { ProductService } from '../../services/product-service';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [ProductForm, CommonModule, TableModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css'
 })
-export class ProductList {
+export class ProductList implements OnInit {
 
+  products = signal<ProductModel[]>([]);
+  editing = signal<ProductModel | undefined>(undefined);
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.load();
+  }
+
+  load() {
+    this.productService.getAllProducts().subscribe(
+      list => this.products.set(list)
+    );
+  }
+
+  onEdit(product: ProductModel) {
+    this.editing.set(product);
+  }
+
+  onNewProduct() {
+    this.editing.set({
+      _id: undefined!,
+      name: '',
+      description: '',
+      price: 0,
+      stock: 0,
+      featured: false,
+      type: '',
+      height: 0,
+      width: 0,
+      thickness: 0
+    });
+  }
+
+  onSaved() {
+    this.editing.set(undefined);
+    this.load();
+  }
+
+  onDelete(id: string) {
+    if (!id) {
+      console.warn('Product id is undefined');
+      return;
+    }
+    if (confirm('Estás seguro de querer borrar este producto?')) {
+       this.productService.deleteProduct(id).subscribe({
+        next: () => this.load(),
+        error: (err) => console.error(err)
+      });
+    }
+  }
 }
